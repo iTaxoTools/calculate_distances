c136f2833cf6dbbf8202dd1ca9b3c82473099ab6
StefanPatman <stefanpatman91@gmail.com>
2022-10-05 16:10:44 +0300

    Fix tkinter resource loading

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="taxi3",
-    version="0.2.0",
+    version="0.2.dev0",
     description="taxi3 description",
     long_description=long_description,
     long_description_content_type="text/markdown",
