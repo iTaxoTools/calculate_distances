e7f91c4e29a1c730fe9e8170e41f36d34b6f466b
Stefanos Patmanidis <stefanpatman91@gmail.com>
2023-04-18 17:05:46 +0300

    Change version to 2.1.dev1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="taxi2",
-    version="2.1.0",
+    version="2.1.dev1",
     description="TaxI2 - Calculation and analysis of pairwise sequence distances",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -51,7 +51,7 @@ setup(
     ],
     python_requires=">=3.9, <4",
     install_requires=[
-        "itaxotools-common>=0.2.4",
+        "itaxotools-common==0.3.dev0",
         "DNAconvert==0.2.0",
         "spart_parser==0.1.1",
         "BioPython>=1.80",
